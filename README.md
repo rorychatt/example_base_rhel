19ec950bda48aa33975a7c013e181a88825be0b0
Rory Chatterton <rchatterton@westpac.com.au>
2017-10-18 22:22:16 +1100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# wbc_rhel_base
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: wbc_rhel_base
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
